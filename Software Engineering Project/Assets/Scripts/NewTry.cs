0035036af7e305d84a36188dd4bab9277183b367
balslamdank <46912062+balslamdank@users.noreply.github.com>
2019-04-05 12:43:36 -0400

    Fixed Audio Bug
    Fixed bug where audio gameobject would detach itself from mute button.

---
+++ Software Engineering Project/Assets/Scripts/NewTry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTry : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("music");
+        if (obj.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Software Engineering Project/Assets/Scripts/WatDaHeck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatDaHeck : MonoBehaviour
+{
+    public void MuteDaMusic()
+    {
+        GameObject obj = GameObject.FindGameObjectWithTag("music");
+
+        obj.GetComponent<AudioSource>().mute = !obj.GetComponent<AudioSource>().mute;
+    }
+}
