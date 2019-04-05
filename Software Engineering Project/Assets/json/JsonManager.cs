fc0e56acf2839f86c58b918ca2ed048f0a25d081
cking97 <im2cool79@gmail.com>
2019-04-04 21:21:34 -0400

    Added Json Files and Manager
    The json files and the one previous manager part that does stuff now that its files.

---
+++ Software Engineering Project/Assets/json/JsonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class JsonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string getData(string type, string name)
+    {
+        string filepath = "./" + type;
+        if (File.Exists(filepath))
+        {
+            string json = File.ReadAllText(filepath);
+            return json; // using JsonUtility.FromJson< MyClass>(data); to build the data after it is returned. 
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
