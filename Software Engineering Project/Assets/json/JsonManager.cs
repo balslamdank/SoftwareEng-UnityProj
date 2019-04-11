7466daa1f404342c32dde858585f9b27c5fcf7d6
cking97 <im2cool79@gmail.com>
2019-04-11 16:19:52 -0400

    Json stuff

--- Software Engineering Project/Assets/json/JsonManager.cs
+++ Software Engineering Project/Assets/json/JsonManager.cs
@@ -8,26 +8,37 @@ public class JsonManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public string getData(string type, string name)
+    public object[] GetData(string type)
     {
         string filepath = "./" + type;
         if (File.Exists(filepath))
         {
             string json = File.ReadAllText(filepath);
-            return json; // using JsonUtility.FromJson< MyClass>(data); to build the data after it is returned. 
+            return this.FormatJson(json, type); // remember to cast the json to the type desired when you get it
         }
         else
         {
-            return "";
+            return this.FormatJson("", "");
+        }
+    }
+
+    public object[] FormatJson(string json, string type) // recall to cast the json to the type desired when you get it
+    {
+        // need a way to deal with the tables that exist in some things
+        if (type == "spells" || type == "magic_items" || type == "items")
+        {
+            return JsonUtility.FromJson<ContentOnlyItem[]>(json);
+        }
+        else
+        {
+            return null;
         }
     }
 }
---
+++ Software Engineering Project/Assets/json/JsonObjectTypes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table: MonoBehaviour
+{
+    // [key: string] string; // this is not how it works in C#
+    // string [key: string]; // nor this
+}
+
+public class ContentOnlyItem : MonoBehaviour
+{
+    string name;
+    string[] content;
+}
