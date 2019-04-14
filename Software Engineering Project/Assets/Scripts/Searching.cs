d7a66b153a63dcdb110ccc21f3d5542ade975067
balslamdunk <33791943+balslamdunk@users.noreply.github.com>
2019-04-14 14:03:30 -0400

    Throwing it back to you John!

--- Software Engineering Project/Assets/Scripts/Searching.cs
+++ Software Engineering Project/Assets/Scripts/Searching.cs
@@ -8,7 +8,7 @@ public class Searching : MonoBehaviour
 {
     private IEnumerator coroutine;
     private bool isRunning = false;
-
+	[SerializeField] string fileSearchName;
     public void FireOffSearch()
     {
         //Cancel other LetterFinds if they exist
@@ -27,7 +27,23 @@ public class Searching : MonoBehaviour
     {
         isRunning = true;
         string search = this.gameObject.GetComponent<InputField>().text;
-        
+
+        string path = Application.dataPath + "/json" + "/" + fileSearchName;
+		string jsonSearchString = File.ReadAllText(path);
+		JSONObject searchJSON = (JSONObject)JSON.Parse(jsonSearchString);
+
+		string[] searchResults = searchJSON[search];
+
+		foreach( string query in searchJSON)
+		{
+			if (query.contains(search))
+			{
+				//Return JSON array 
+			}
+		}
+
+	
+
         //Search through (list? JSON file?)
 
     }
