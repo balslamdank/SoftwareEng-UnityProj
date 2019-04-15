838ba310cb0a39a0f98717d89086da1b7a8a041f
balslamdank <46912062+balslamdank@users.noreply.github.com>
2019-04-15 00:12:54 -0400

    Search Populates Buttons
    Searching for an item will populate the buttons.

--- Software Engineering Project/Assets/Scripts/Searching.cs
+++ Software Engineering Project/Assets/Scripts/Searching.cs
@@ -11,6 +11,7 @@ public class Searching : MonoBehaviour
     private IEnumerator coroutine;
     private bool isRunning = false;
 	[SerializeField] string fileSearchName;
+    public GameObject buttons;
     public void FireOffSearch()
     {
         //Cancel other LetterFinds if they exist
@@ -34,16 +35,24 @@ public class Searching : MonoBehaviour
         string jsonSearchString = File.ReadAllText(path);
         JSONObject searchJSON = (JSONObject)JSON.Parse(jsonSearchString);
         string test = searchJSON.ToString();
+        List<List<string>> myList = new List<List<string>>();
 
         JObject parsed = JObject.Parse(test);
+        int i = 0;
 
         foreach (var pair in parsed)
         {
             if(pair.Key.ToLower().Contains(search.ToLower()))
             {
                 Debug.Log(pair.Key);
+                myList.Add(new List<string> {pair.Key, pair.Value.ToString()});
             }
+        }
 
+        foreach(Transform child in buttons.transform)
+        {
+            child.GetChild(0).GetComponent<Text>().text = myList[i][0];
+            i++;
         }
 
         yield return null;
